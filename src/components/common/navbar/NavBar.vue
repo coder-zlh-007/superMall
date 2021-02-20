cdad265f7d399e769a9c3ca11118585f7ff8e6a9
zlh <3211187537@qq.com>
2021-02-21 07:25:53 +0800

    首页导航条

--- src/components/common/navbar/NavBar.vue
+++ src/components/common/navbar/NavBar.vue
@@ -1,5 +1,9 @@
 <template>
-  <div></div>
+  <div class="nav-bar">
+    <div class="left"><slot name="left"></slot></div>
+    <div class="center"><slot name="center"></slot></div>
+    <div class="right"><slot name="right"></slot></div>
+  </div>
 </template>
 
 <script>
@@ -9,4 +13,20 @@ export default {
 
 </script>
 <style scoped>
+  .nav-bar{
+    display: flex;
+    height: 44px;
+    line-height: 44px;
+    text-align: center;
+    box-shadow: 0px 1px 1px rgba(100, 100, 100, .1);
+  }
+
+  .left, .right {
+    width: 60px;
+  }
+
+  .center{
+    flex: 1;
+  }
+
 </style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,38 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="nav-bar">
+      <div slot="center">购物车</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
-export default {
+import NavBar from "@/components/common/navbar/NavBar"
+import {getHomeMultiData} from "@/network/home"
 
+export default {
+  name: "Home",
+  components: {
+    NavBar,
+  },
+  data() {
+    return {
+      banner: null,
+      recommend: null
+    }
+  },
+  created () {
+    getHomeMultiData().then(res => {
+      this.banner = res.data.banner.list;
+      this.recommend = res.data.recommend.list;
+    })
+  },
 }
 </script>
 
 <style>
-
+ .nav-bar{
+   background-color: var(--color-tint);
+   color: #fff;
+ }
 </style>
